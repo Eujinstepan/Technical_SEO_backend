6a63adf59afba719473e11a653873f2aee211137
jiraya001207 <jiraya001207@gmail.com>
2022-06-07 02:24:20 +0900

    update some

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,38 +1,45 @@
 const SEOChecker = require("advanced-seo-checker");
+const Crawler = require("simplecrawler");
 // const pagespeedInsights = require("pagespeed-insights");
-const pagespeed = require('gpagespeed');
+const pagespeed = require("gpagespeed");
 const findOrphans = require("find-orphans");
 const { structuredDataTest } = require("structured-data-testing-tool");
 const parseCanonicalUrl = require("parse-canonical-url");
-const httpschecker = require('https-checker');
-const Slugify = require('seo-friendly-slugify');
-const {
-  Google,
-} = require("structured-data-testing-tool/presets");
+const httpschecker = require("https-checker");
+const Slugify = require("seo-friendly-slugify");
+const { Google } = require("structured-data-testing-tool/presets");
 const { default: axios } = require("axios");
-var DomParser = require('dom-parser');
+var DomParser = require("dom-parser");
+const { Tester } = require("@nickreese/seo-lint");
+const { SEOLinter } = require("seo-linter");
+const blc = require("broken-link-checker");
 
 // const gsc = require("gsc-cli");
- 
+
 module.exports = {
   testApi: async (req, res) => {
     let url = req.query.url;
-    // let baseURL = domain_from_url(url);
     let urls = [url];
-    let crawler = SEOChecker(url, {});
+    let seoChecker = SEOChecker(url, {});
     
-    // const s = new Slugify();
-    // console.log(s.slugify(url));
-    // return;
-
+    var urlChecker = new blc.UrlChecker(null, {
+      error: function(error){console.log('error', error)},
+      queue: function(){console.log('queue')},
+      link: function(result, customData){console.log('link', result)},
+      end: function(){console.log('end')},
+  });
+  
     // Check canonical tag
-    let pageSource = await axios.get(url).then(res => res.data);
+    let pageSource = await axios.get(url).then((res) => res.data);
     const parser = new DomParser();
     let dom = parser.parseFromString(pageSource);
-    let links = dom.getElementsByTagName('link');
+    let links = dom.getElementsByTagName("link");
     let isCanonical = false;
     for (var i = 0; i < links.length; i++) {
-      if (links[i].getAttribute("rel") === "canonical" && links[i].getAttribute("href") === url) {
+      if (
+        links[i].getAttribute("rel") === "canonical" &&
+        links[i].getAttribute("href") === url
+      ) {
         isCanonical = true;
       }
     }
@@ -47,14 +54,14 @@ module.exports = {
       key: "AIzaSyAxAiLsn-bmx6FjxShKifYYNTMf-p5LcvA", //key is in Google cloud Platform key.
     };
     let _pagespeed = await pagespeed(opts)
-      .then(data => {
-        console.log(data)
+      .then((data) => {
+        console.log(data);
         return data;
       })
-      .catch(error => {
-        console.error(error)
+      .catch((error) => {
+        console.error(error);
         return error.errno;
-      })
+      });
 
     // Add structured data
     let _structuredata = await structuredDataTest(url, {
@@ -97,21 +104,37 @@ module.exports = {
       });
 
     // Leverage "Inspect URL" feature in GSC, Ensure your website is mobile-friendly
-    let summary = await crawler.analyze(urls).then(function (summary) {
+    let summary = await seoChecker.analyze(urls).then(function (summary) {
       console.log("..................");
       console.log(">>>>>>", summary);
       return summary;
     });
-    console.log(summary.pages[0]["isMobileFriendly"], "justin")
+    console.log(summary.pages[0]["isMobileFriendly"], "justin");
     let isMobileFriendly = summary.pages[0]["isMobileFriendly"];
     console.log("ismobilefriendly", isMobileFriendly);
-    res.status(200).json({
-      leverage: summary,
-      isMobileFriendly: isMobileFriendly,
-      pagespeed: _pagespeed,
-      canonical: isCanonical,
-      Ishttpsurl: isHTTPSUrl,
-      StructureData: _structuredata
-    });
+
+    // Check page depth
+    let fetchResult = [];
+    const crawler = Crawler(url)
+      .on("fetchcomplete", function (queueItem, responseBody, response) {
+        let queueItems = queueItem.depth;
+        fetchResult.push({ queueItem }); 
+        urlChecker.enqueue(url);
+      })
+      .on("complete", function () {
+        res.status(200).json({
+          leverage: summary,
+          isMobileFriendly: isMobileFriendly,
+          crawlerChecked: summary.body[0]["audits"]["is-crawlable"],
+          pagespeed: _pagespeed,
+          canonical: isCanonical,
+          Ishttpsurl: isHTTPSUrl,
+          StructureData: _structuredata,
+          crawlerResult: fetchResult,
+        });
+        console.log(fetchResult);
+      });
+    crawler.maxDepth = 10;
+    crawler.start();
   },
 };
